00620070ea111cb17ba4935d02f33c4736d0bfa9
Mayra <mayra203@gmail.com>
2023-12-10 22:09:38 -0600

    Import compmonents and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import searchIcon from './assets/search.svg'
+import { useCities } from './hooks/useCities'
+import { useSearch } from './hooks/useSearch'
+import { Cities } from './components/Cities'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {search, setSearch, error} = useSearch()
+  const {cities, loading, getCities} = useCities({search})
+  
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    getCities({cities})
+  }
+  
+  const handleChange = (event)=>{
+    const newSearch = event.target.value;
+    setSearch(newSearch)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container text-center">
+        <form action="form" onSubmit={handleSubmit}>
+          <div className="row align-items-start">
+            <label htmlFor="exampleFormControlInput1" className="form-label">Buscador</label>
+          </div>
+          <div className="row g-3 align-items-center">
+            <div className="col-auto">
+              <input style={{border: '1px solid transparent', borderColor: error ? 'red' : 'grey'}} type="search" onChange={handleChange} className="form-control" id="search" placeholder='México, Monterrey...'/>
+            </div>
+            <div className="col-auto">
+              <button className='btn btn-default'><img src={searchIcon} width={30} height={30} alt="Imágen lupa" /></button>
+            </div>
+          </div>
+        </form>
+        <main>
+          {
+            loading ? <p>Cargando...</p> : <Cities cities={cities} />
+          }
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
